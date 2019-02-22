8f40309775eb70e8b30d6ec10c71723a6e45132d
Connor Kim(김두준) <43170570+DoojoonKim@users.noreply.github.com>
2019-02-22 20:08:55 +0900

    Update ERC20(Connor).sol

--- ERC20(Connor).sol
+++ ERC20(Connor).sol
@@ -5,16 +5,8 @@ import "./SafeMath(Connor).sol";
 
 /**
  * @title Standard ERC20(Connor) token
- *
  * @dev Implementation of the basic standard token.
- * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
- * Originally based on code by FirstBlood:
- * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- *
- * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
- * all accounts just by listening to said events. Note that this isn't required by the specification, and other
- * compliant implementations may not do it.
- */
+
 contract ERC20_Connor is IERC20_Connor {
     using SafeMath_Connor for uint256;
 
@@ -65,7 +57,6 @@ contract ERC20_Connor is IERC20_Connor {
      * Beware that changing an allowance with this method brings the risk that someone may use both the old
      * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
      * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
      * @param spender The address which will spend the funds.
      * @param value The amount of tokens to be spent.
      */
